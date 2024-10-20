711b4044fc1f1092c5f1292174e1f36ad2c9d57f
mohamed <mohamedsamirtkd@gmail.com>
2024-10-20 03:48:15 +0300

    Create setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="estats",
+    version="0.1",
+    packages=find_packages(),  # Automatically finds all packages and sub-packages
+    install_requires=[],  # Optional: List dependencies if any
+)
