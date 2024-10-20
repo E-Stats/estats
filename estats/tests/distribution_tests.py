1e524e0fc5974058bf3120adbc8995945dd30dd7
omarghanem1210 <omarghanem1210@gmail.com>
2024-10-20 19:46:30 +0300

    normality test

--- estats/tests/distribution_tests.py
+++ estats/tests/distribution_tests.py
@@ -1,10 +1,7 @@
-from cor import dcor
 import numpy as np
-from scipy.stats import zscore, norm, cauchy, kstest, t
-from sklearn import datasets
-
-iris = datasets.load_iris()['data']
-
+from scipy.stats import zscore, norm
+from scipy.special import hyp1f1, gamma
+from scipy.spatial import distance_matrix
 
 def normality_statistic(x: np.array) -> np.float64:
     """
@@ -12,36 +9,44 @@ def normality_statistic(x: np.array) -> np.float64:
     """
     x = np.array(x)
     n = len(x)
-    s = np.std(x, ddof=1) 
-        
-    y = np.sort((x - np.mean(x)) / s) 
+
+    y = np.sort(zscore(x, ddof=1)) 
     k = np.arange(1 - n, n, 2)
     
-    statistic = 2 * (np.sum(2 * y * norm.cdf(y) + 2 * norm.pdf(y)) - 
-                  n/np.sqrt(np.pi) - 
-                  np.mean(k * y))
+    statistic = 2 * (np.sum(2 * y * norm.cdf(y) + 2 * norm.pdf(y)) - n/np.sqrt(np.pi) - np.mean(k * y))
     
     return statistic
+
+def m_normality_statistic(x: np.array) -> np.float64:
+    """
+    Returns the statistic for the multivariate normality test
+    """
+    n = len(x)
+    y = zscore(x)
+    d = x.shape[1]
+    neg_norms = - (np.linalg.norm(y, axis=1)**2 / 2)
+    k = gamma((d+1) / 2) / gamma(d/2)
+
+    first_term = np.sqrt(2) * k * (2/n) * np.sum(hyp1f1(-1/2, d/2, neg_norms))
+    second_term = 2 * k +  np.sum(distance_matrix(y, y)) / n**2
+
+    return n * (first_term - second_term)
+
     
 
-def normality_etest(x: np.array, permutations: int = 1000) -> dict:
+def normality_etest(x: np.array, iterations: int = 1000, statistic = normality_statistic) -> dict:
     """
-    Given a vector x, tests whether that vector is normal and returns the test statistic and p-value in a dictionary
+    Given a data matrix x which is n * m, tests whether the  m random variable are normal and returns the test statistic and p-value in a dictionary
     """
-    observed_statistic = normality_statistic(x)
+    statistic = normality_statistic
+    if np.ndim(x) > 1:
+        statistic = m_normality_statistic
+    observed_statistic = statistic(x)
     simulations = []
-    n = len(x)
 
-    for i in range(permutations):
-        sim_x = norm.rvs(size = n)
-        simulations.append(normality_statistic(sim_x))
+    for i in range(iterations):
+        sim_x = norm.rvs(size = x.shape)
+        simulations.append(statistic(sim_x))
 
     p_value = np.mean(simulations > observed_statistic) 
     return {'observed_statistic': observed_statistic, 'p_value': p_value}
-
-
-
-x = t.rvs(5, size = 1000)
-print(f"etsest: {normality_etest(x)}")
-print(f'kolmogrov test: {kstest(x, cdf=norm.cdf)}')
-
