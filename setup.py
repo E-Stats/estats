23e23d8fae5adf1ea72446bc1bfbc76558d24f05
Faidullah Moftah <faidullamoftah2@gmail.com>
2025-06-10 11:36:03 +0300

    Made dependencies explicit

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="estats",
     version="0.1",
-    packages=find_packages(),  # Automatically finds all packages and sub-packages
-    install_requires=[],  # Optional: List dependencies if any
+    packages=find_packages(),  
+    install_requires=[
+        "numpy>=1.21.0",
+        "scipy>=1.7.0",
+        "scikit-learn>=0.24.0",
+        "pandas>=1.3.0",
+        "matplotlib>=3.4.0"
+    ],
 )
